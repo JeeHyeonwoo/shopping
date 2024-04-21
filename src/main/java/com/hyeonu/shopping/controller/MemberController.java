9a9d99b05453ad94fbd54d344a53ce69f6c46224
Hyeonwoo Jee <gusdn242800@gmail.com>
2024-04-21 16:55:37 +0900

    update: 브랜드 신청 기능

--- src/main/java/com/hyeonu/shopping/controller/MemberController.java
+++ src/main/java/com/hyeonu/shopping/controller/MemberController.java
@@ -67,7 +67,6 @@ public class MemberController {
                     .localDateTime(LocalDateTime.now().toString())
                     .path(request.getRequestURI())
                     .message(map)
-                    .Status(409)
                     .build();
         }
 
@@ -76,7 +75,6 @@ public class MemberController {
                 .localDateTime(LocalDateTime.now().toString())
                 .path(request.getRequestURI())
                 .message(map)
-                .Status(200)
                 .build();
     }
 }
